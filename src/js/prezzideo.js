833a2673102a225c8bdb83628a211ed5f6ac8674
frenski <frenski@gmail.com>
2015-08-12 15:05:29 +0200

    Added screens positioning

--- src/js/prezzideo.js
+++ src/js/prezzideo.js
@@ -13,16 +13,27 @@
 	var config;
 	
 	var defaults = {
-		dom:{
-			container:'.prezzideo'
+		dom: {
+			container:'.prezzideo',
+			slidesContainer: '.prezzideo-slides',
+			slideItem:'.prezzideo-slides-item',
+			slideVClass: 'prezzideo-slide-vertical',
+			slideHClass: 'prezzideo-slide-horizontal',
+			slidesShrinkedClass: 'prezzideo-slides-shrinked',
+			videoContainer: '.prezzideo-video',
+			videoShrinkedClass: 'prezzideo-video-shrinked',
+			screenPositionClass: 'prezzideo-screen-position-'
 		},
-		callbackInit:null,
-		callbackDistroy:null,
-		videoProvider:'youtube'
+		autoplay: false,
+		callbackInit: null,
+		callbackDistroy: null,
+		videoProvider: 'youtube',
+		defaultBigScreen: 'slides',
+		smallScreenPosition: 'bottom-right'
 	};
 	
 	var vProvidersEmbedCode = {
-		'youtube':'<iframe width="{{w}}" height="{{h}}" src="https://www.youtube.com/embed/{{id}}" frameborder="0"></iframe>'
+		'youtube':'<iframe src="https://www.youtube.com/embed/{{id}}?autoplay={{auto}}&controls=0&modestbranding=1&showinfo=0" frameborder="0"></iframe>'
 	}
 	
 
@@ -33,7 +44,7 @@
 	// Extending the defaults config
 	// Using Deep extend two objects. Legacy:
 	// http://andrewdupont.net/2009/08/28/deep-extending-objects-in-javascript/
-	var _extendConfig = function( destination, source ) {
+	var _extendConfig = function(destination, source) {
         for (var property in source) {
             if (source[property] && source[property].constructor 
 					&& source[property].constructor === Object) {
@@ -47,6 +58,37 @@
         return destination;
 	}
 	
+	// A function which allows for image preloading
+	var _loadImages = function(imgs, imgsReturn, loadCallback) {
+
+		// Creates a new image object with src of the dom image
+		var img = new Image();
+		img.src = imgs[0].getAttribute('src');
+		// shifts the array until it's empty
+		if( imgs.length > 1 ) { 
+	 		img.onload = function() {
+				imgsReturn.push(imgs[0]);
+				imgs.shift(); 
+				// recursively calls the function with the updated array 
+				_loadImages(imgs, imgsReturn, loadCallback); 
+			}
+		} else {
+			img.onload = function() {
+				imgsReturn.push(imgs[0]);
+				if(typeof loadCallback === 'function') {
+				    loadCallback();
+				}
+			}
+		}
+
+	}
+	
+	
+	// A generic function to add html markup do a dom element
+	var _insertHTML = function() {
+		
+	}
+	
 	
 	// The player class
 	function Prezzideo(element,id) {
@@ -54,6 +96,8 @@
         var self = this;
         self.element = element;
 		self.playerId = id;
+		self.slides = [];
+		self.currentSlide = 0;
 		
 		// A funciton to select a child dom element by selector
 		var _selectChild = function(selector) {
@@ -63,10 +107,6 @@
 		// A funciton to select children dom elements by selector
 		var _selectChildren = function(selector) {
 			return self.element.querySelectorAll(selector);
-		}
-		
-		var _insertHTML = function(){
-			
 		}
 		
 		// A crossbrowser funciton to handle events addition
@@ -87,6 +127,89 @@
 			}
 		}
 		
+		
+		var _insertControls = function() {
+			
+		}
+		
+		
+		// This function inits which of the screen is on the top and what
+		// is the default position of the each screen: top, right, bottom, left
+		var _initPositions = function() {
+			
+			_changeSmallScreenPosition(config.smallScreenPosition);
+			
+			if (config.defaultBigScreen == 'video') {
+				var slideC = _selectChild(config.dom.slidesContainer);
+				slideC.className += ' ' + slidesShrinkedClass;
+			} else if (config.defaultBigScreen == 'slides') {
+				var videoC = _selectChild(config.dom.videoContainer);
+				videoC.className += ' ' + config.dom.videoShrinkedClass;
+			}
+			
+		}
+		
+		
+		var _changeSmallScreenPosition = function( position ) {
+			var slideC = _selectChild(config.dom.slidesContainer);
+			var videoC = _selectChild(config.dom.videoContainer);
+			slideC.className += ' ' +config.dom.screenPositionClass + position;
+			videoC.className += ' ' +config.dom.screenPositionClass + position;
+		}
+		
+		
+		// A function to compare the aspect ratio of the container and the 
+		// images and place the image depending on that and also make the
+		// latest ones invisible
+		// TODO: add some sorting function
+		var _resetImages = function() {
+			var cRatio = self.element.clientWidth / self.element.clientHeight;
+			for (var i = 0; i < self.slides.length; i ++ ){
+				var iRatio = self.slides[i].clientWidth / 
+							 self.slides[i].clientHeight;
+				if (cRatio <= iRatio) {
+					// Adds vertical alignment of the class, since it's wider
+					// than the container
+					self.slides[i].className += ' ' + config.dom.slideVClass;
+					var marginTop = - self.slides[i].clientHeight / 2;
+					self.slides[i].style.marginTop = marginTop + 'px';
+				} else {
+					// Adds vertical alignment of the class, since it's higher
+					// than the container
+					self.slides[i].className += ' ' + config.dom.slideHClass;
+					var marginLeft = - self.slides[i].clientWidth / 2;
+					self.slides[i].style.marginLeft = marginLeft + 'px';
+				}
+				if (i > 0 ) self.slides[i].style.display = 'none';
+			}
+		}
+		
+		
+		// Gets the slides depending on the attributes and sends the array
+		// for loading
+		var _getSlides = function(loadCallback) {
+			var images = [];
+			var slides = _selectChildren(config.dom.slideItem);
+			for (var i = 0; i < slides.length; i ++) {
+				if (slides[i]!=null){
+					if(slides[i].getAttribute('data-time')){
+						images.push(slides[i]);
+					}
+				}
+			}
+			_loadImages(images, self.slides, loadCallback);
+		}
+		
+		// Adds the video markup to the container, depending on the provider
+		var _addVideo = function() {
+			var videoMarkup = '<div class="prezzideo-video">'
+				+ vProvidersEmbedCode[config.videoProvider]
+					.replace('{{id}}', self.element.getAttribute('data-urlid'))
+					.replace('{{auto}}', config.autoplay)
+				+ '</div>';
+			self.element.innerHTML += videoMarkup;
+		} 
+		
 		// A function to play the video
 		var _play = function() {
 			
@@ -99,12 +222,17 @@
 		
 		// The function to be called on init
 		var _init = function() {
-			console.log('init player'+self.playerId);
+			_addVideo();
+			_getSlides(function(){
+				_resetImages();
+			});
+			_initPositions();
+			// console.log('init player'+self.playerId);
 		}
 		
 		// The function to be called on init
 		var _destroy = function() {
-			console.log('some destroy function');
+			// console.log('some destroy function');
 		}
 		
 		// Inits or returns empty object if init fails
@@ -136,7 +264,7 @@
 		var players = [];
 
 		// Initializing Prezzideo instance for each DOM element
-		for (var i = 0; i < elements.length; i++) {
+		for (var i = 0; i < elements.length; i ++) {
 		
 			// Setup a player instance and add to the element
 			if(typeof elements[i].prezzideo === "undefined") { 
